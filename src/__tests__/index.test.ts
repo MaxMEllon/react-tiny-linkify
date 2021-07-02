556181cd50061343a28b88f605b95ba81429881d
maxmellon <tsujiken_melon@r.recruit.co.jp>
2021-07-02 20:43:56 +0900

    chore: fmt

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/server'
-import linkfy from '../index.js'
+import React from "react";
+import ReactDOM from "react-dom/server";
+import linkfy from "../index.js";
 
-describe('basic usage', () => {
-  it('works', () => {
+describe("basic usage", () => {
+  it("works", () => {
     const s = ReactDOM.renderToStaticMarkup(
-      React.createElement('p', {}, linkfy('sample http://google.com contents'))
-    )
-    console.log(s)
-  })
-})
+      React.createElement("p", {}, linkfy("sample http://google.com contents")),
+    );
+    console.log(s);
+  });
+});
