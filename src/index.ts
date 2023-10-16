df63abda87712d3f8a7a43a0ddfd139d47e00876
midoghranek <midoghranek@hotmail.com>
2023-10-16 12:42:38 +0300

    🐛 FIX: regexp is reading decimal numbers as url

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Anchor as DefaultAnchor } from "./components/anchor";
 
 const regexp =
-  /(https?:\/\/)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9]{1,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/ig
+  /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/ig
 
 export default function linkify(
   text: string,
