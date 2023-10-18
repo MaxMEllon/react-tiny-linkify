8e1714feb371468d88cb44f3a59e8b667ddd23dd
MaxMEllon <maxmellon1994@gmail.com>
2023-10-18 10:31:23 +0900

    chore: update dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   transform: {
-    ".(ts|tsx)": "ts-jest"
+    ".(ts|tsx)": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json"
+      }
+    ]
   },
   testPathIgnorePatterns: ["/node_modules/"],
   testRegex: "(/__test__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
   moduleFileExtensions: ["ts", "tsx", "js", "json"],
   testEnvironment: "node",
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json"
-    }
-  }
 };
